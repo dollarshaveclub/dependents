59131b1d4840d34681a3acf9f18780dbe769662a
Jacob Kelley <jakie8@gmail.com>
2019-02-19 11:25:10 -0800

    Plain text output

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const colors = require('colors')
+let colors = require('colors')
 const program = require('commander')
 const path = require('path')
 
@@ -8,10 +8,22 @@ const { dependentPackages } = require('../lib')
 
 program
   .version(version)
+  .option('-m, --machine-readable', 'Allow for machine-readable output')
+  .option('-t, --text', 'Plain text output')
   .option('-p, --package [file]', 'The path to the package.json file that contains dependents')
   .option('-r, --remote', 'Search against remote repositories in your org')
   .option('-i, --installed', 'Only show packages that have the dependency installed')
   .parse(process.argv)
+
+if (program.t) {
+  colors = {
+    cyan: (s) => s,
+    yellow: (s) => s,
+    bold: (s) => s,
+    grey: (s) => s,
+    red: (s) => s,
+  }
+}
 
 const packagePath = program.package || path.join(process.cwd(), 'package.json')
 const token = process.env.GITHUB_TOKEN
@@ -19,6 +31,10 @@ if (!token) throw new Error(`\nEnvironment variable GITHUB_TOKEN must be set.\nG
 
 dependentPackages(packagePath, program.remote, token)
   .then(({ clientPackage, dependentPackages }) => {
+    if (program.m) {
+      console.log(dependentPackages.map(({ data: { name, version } }) => ({ name, version })))
+      return process.exit()
+    }
     console.log(`${colors.bold(clientPackage.name)}: ${colors.cyan(colors.bold(clientPackage.version))}`)
     dependentPackages
       .sort((a, b) => a.data.name > b.data.name)
@@ -32,5 +48,6 @@ dependentPackages(packagePath, program.remote, token)
 
         if ((program.installed && version) || !program.installed) console.log(`${prefix}${repo}: ${note ? colors.yellow(note) : version || colors.grey('Not Installed')}`)
       })
-  })
+  }
+)
   .catch(e => console.error(colors.red(e.message)))
